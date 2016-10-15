62fcd743043c297d38447da9bc5e225c45349547
Sheila Fatanihza Safitri <sheilafatanihzasafitri@gmail.com>
2016-10-15 21:50:14 +0700

    Radio Button

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl435/projectkos/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl435/projectkos/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNama;
     Button bOK;
     TextView tvHasil;
+    RadioButton rbW, rbL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,42 @@ public class MainActivity extends AppCompatActivity {
 
         etNama = (EditText)findViewById(R.id.editTextNama);
         bOK = (Button) findViewById(R.id.buttonOK);
+        rbW = (RadioButton) findViewById(R.id.radioButtonWa);
+        rbL = (RadioButton) findViewById(R.id.radioButtonLa);
+
         tvHasil = (TextView)findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                doClick();
+            }
+        });
 
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String nama = etNama.getText().toString();
-                tvHasil.setText("Nama : "+nama);
+            public void onClick(View view) {
+                doClick();
             }
         });
+    }
+
+    private void doClick() {
+        String hasil = null;
+        String nama = etNama.getText().toString();
+
+        if(rbW.isChecked()){
+            hasil = rbW.getText().toString();
+        }
+        else {
+            hasil = rbL.getText().toString();
+        }
+
+        if (hasil == null){
+            tvHasil.setText("Nama : " +nama+ "\n Belum memilih jenis kelamin");
+        }
+        else{
+            tvHasil.setText("Nama : "+nama+"\n Jenis kelamin anda : "+hasil);
+        }
     }
 }
