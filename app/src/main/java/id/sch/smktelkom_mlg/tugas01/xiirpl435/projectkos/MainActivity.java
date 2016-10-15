785c5f80e87774395b4a6e60aa70463972a4c5f3
Sheila Fatanihza Safitri <sheilafatanihzasafitri@gmail.com>
2016-10-15 17:43:49 +0700

    input nama

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl435/projectkos/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl435/projectkos/MainActivity.java
@@ -2,12 +2,31 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl435.projectkos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    Button bOK;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText)findViewById(R.id.editTextNama);
+        bOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView)findViewById(R.id.textViewHasil);
+
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etNama.getText().toString();
+                tvHasil.setText("Nama : "+nama);
+            }
+        });
     }
 }
